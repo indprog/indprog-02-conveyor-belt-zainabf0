3e44a0f515d553330d23c4fbed94c26a952b0e3b
ZainabF0 <s117010@dtu.dk>
2026-09-11 21:03:42 +0200

    conveyor-belt-capacity check.c

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello!")
+motors = int(input("How many motors are carrying the packages?"))
+package_weight = int(input("How many kg of packages do we expect?"))
+
+weight_per_motor = package_weight / motors
+
+if weight_per_motor <= 12:
+    print("Yes! The conveyor belt can carry the weight.")
+else:
+    print("No! The conveyor belt cannot carry the weight.") 
