335ef03aada11213925133a2747a0776c6c89d4b
ZainabF0 <s117010@dtu.dk>
2026-09-14 15:27:59 +0200

    Update print statements for package handling

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ package_weight = int(input("How many kg of packages do we expect?"))
 weight_per_motor = package_weight / motors
 
 if weight_per_motor <= 12:
-    print("Yes! The conveyor belt can carry the weight.")
+    print("Yes! The conveyor belt can carry the packages.")
 else:
-    print("No! The conveyor belt cannot carry the weight.") 
+    print("No! The conveyor belt cannot carry the packages.") 
